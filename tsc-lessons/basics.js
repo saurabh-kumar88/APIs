847398bfca5d8cdfb6ba5c57bc723d21341cc463
saurabh-kumar88 <ykings.saurabh@gmail.com>
2021-01-12 00:19:24 +0530

    second

---
+++ tsc-lessons/basics.js
@@ -0,0 +1,10 @@
+// hello world
+var isDone = true;
+var message = "hello world";
+// console.log(isDone);
+var decimal = -6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+// let big: bigint = 100n;
+console.log(typeof decimal);
---
+++ tsc-lessons/basics.ts
@@ -0,0 +1,14 @@
+// hello world
+let isDone: Boolean = true;
+let message: string = "hello world";
+
+// console.log(isDone);
+
+
+let decimal: number = -6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+// let big: bigint = 100n;
+
+console.log(typeof decimal);
